3a83d28eb07211d08a29c75f747ec4b59119356b
LeeJinWoo-web <aoel900202@gmail.com>
2021-03-22 13:13:21 +0900

    Add Tweet Create Read

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,22 @@
-import {React, useState}  from 'react';
+import {React, useEffect, useState}  from 'react';
 import { dbService } from 'fbase';
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async() => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach(document => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id    
+            }
+            setNweets((prev) => [nweetObject, ...prev])
+        })
+    }
+    useEffect(() => {
+        getNweets();
+    }, [])
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -29,6 +44,11 @@ const Home = () => {
             />
             <input type="submit" value="Nweet"/>
         </form>
+        {nweets.map(nweet => 
+                <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>
+            )}
     </div>
     )
 }
