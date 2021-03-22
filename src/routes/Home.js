c165e3041eefb355d5c3527b2ca8b2db814ee92b
LeeJinWoo-web <aoel900202@gmail.com>
2021-03-22 11:55:08 +0900

    Add firebase Database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import {React, useState}  from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
 
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const onChange = (event) => {
+        const { target:{ value }, } = event; 
+        setNweet(value);
+    }
+
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input 
+            value={nweet}
+            type="text" 
+            placeholder="what's on your mind?" 
+            maxLength={120} 
+            onChange={onChange}
+            />
+            <input type="submit" value="Nweet"/>
+        </form>
+    </div>
+    )
+}
 export default Home;
