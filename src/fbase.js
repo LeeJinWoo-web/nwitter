c384d1ff6c9260c850eb5598f14852c945065e22
LeeJinWoo-web <aoel900202@gmail.com>
2021-03-22 12:16:36 +0900

    Create Tweets

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth"
-import "firebase/database"
+import "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -17,3 +17,5 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
+
+export const dbService = firebase.firestore();
