abd2af3672e56ea4d34ccfc93460fe485b18b38c
LeeJinWoo-web <aoel900202@gmail.com>
2021-03-22 15:06:12 +0900

    Update From isLoggedIn State Delete

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,18 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setLoggedIn(true);
         setUserObj(user)
-      } else {
-        setLoggedIn(false);
       }
       setInit(true);
     })
   }, [])
     return (
       <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/> : "initializing..." }
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/> : "initializing..." }
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
       </>
     );
