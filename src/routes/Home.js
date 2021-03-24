a3cc046064e393fc5f35fb0d6b589262cb1c1d80
LeeJinWoo-web <aoel900202@gmail.com>
2021-03-24 17:55:32 +0900

    Profile setting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,7 @@ import Nweet from 'components/Nweet';
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    const [attachment, setAttachment] = useState();
+    const [attachment, setAttachment] = useState("");
 
     // const getNweets = async() => {
     //     const dbNweets = await dbService.collection("nweets").get();
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,26 @@
-import { authService } from 'fbase';
-import React from 'react';
+import { authService, dbService } from 'fbase';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
 
 const Profile = () => <span>Profile</span>;
 
-export default () => {
+export default ({ userObj }) => {
     const history = useHistory();
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/")
     }
+    const getMyNweets = async () => {
+        const nweets = await dbService
+        .collection("nweets")
+        .where("creatorId", "==", userObj.uid)
+        .orderBy("createdAt")
+        .get();
+        console.log(nweets.docs)
+    }
+    useEffect(() => {
+        getMyNweets();
+    }, [])
     return (
         <>
         <button onClick={onLogOutClick}>Log Out</button>
